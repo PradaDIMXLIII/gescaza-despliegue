94ea91bcf677193e021e273525b4ed58295762bc
Miguel Prada <Alkoliahmpm.mp@gmail.com>
2021-03-29 20:41:07 +0200

    "commit inicial"

---
+++ src/main/java/es/mde/gescaza/GescazaApplication.java
@@ -0,0 +1,13 @@
+package es.mde.gescaza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GescazaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GescazaApplication.class, args);
+	}
+
+}
