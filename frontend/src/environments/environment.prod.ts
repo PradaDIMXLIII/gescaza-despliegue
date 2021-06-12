6e9b8f6a843e5d330ca77512447dc4b693ea7d40
Miguel Prada <Alkoliahmpm.mp@gmail.com>
2021-06-12 22:50:39 +0200

    "modificando environment.prod.ts"

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: true,
+  appAuthor: 'MPM',
+  user: 'user',
+  password: 'password',
+  autenticacion: false,
+  appName: 'Gescaza-App',
   host: 'https://gescaza.herokuapp.com/api/'
 };
